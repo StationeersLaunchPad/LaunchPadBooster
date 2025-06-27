c0953257943cb93d4ecf09f84775371ce2435596
Tom Holmes <tholmes150@gmail.com>
2025-06-27 14:29:41 -0500

    split out into separate files. switch to iterator based codegen with autoindent

--- LaunchPadBooster.Analyzers/ConnectDataGenerator.cs
+++ LaunchPadBooster.Analyzers/ConnectDataGenerator.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using Microsoft.CodeAnalysis;
@@ -101,165 +102,37 @@ namespace LaunchPadBooster.Generated
             if ((prop.Saved || prop.Networked) && prop.Referencable)
               connectedReferences = true;
           }
-          return new ConnectedClass(
-            Namespace: NamespaceName(clazz.ContainingNamespace),
-            ClassName: clazz.Name,
-            SaveDataType: stypeString,
-            Props: props,
-            FlagBytes: flagBytes,
-            ConnectedReferences: connectedReferences
-          );
+          return new ConnectedClass
+          {
+            Namespace = NamespaceName(clazz.ContainingNamespace),
+            ClassName = clazz.Name,
+            SaveDataType = stypeString,
+            Props = props,
+            FlagBytes = flagBytes,
+            ConnectedReferences = connectedReferences
+          };
         }
       );
 
       context.RegisterSourceOutput(pipeline, static (context, model) =>
       {
-        var source = new StringBuilder();
-        source.Append($@"
-using Comparators = LaunchPadBooster.Generated.Comparators;
-using Serializers = LaunchPadBooster.Generated.Serializers;
-using ThingSaveData = Assets.Scripts.Objects.ThingSaveData;
-using NetworkManager = Assets.Scripts.Networking.NetworkManager;
-using RocketBinaryReader = Assets.Scripts.Networking.RocketBinaryReader;
-using RocketBinaryWriter = Assets.Scripts.Networking.RocketBinaryWriter;
-using XmlElementAttribute = System.Xml.Serialization.XmlElementAttribute;
-");
-        if (model.Namespace != "")
-          source.Append($@"
-namespace {model.Namespace}
-{{");
-        source.Append($@"
-  partial class {model.ClassName}
-  {{
-");
-
-        for (var i = 0; i < model.Props.Count; i++)
+        try
         {
-          var prop = model.Props[i];
-          if (prop.Saved && model.SaveDataType == null)
-            source.AppendLine($"#error {prop.PropName} is Saved but no SaveData type was provided");
-          source.Append(prop.GenerateProperty());
+          context.AddSource($"{model.Namespace}.{model.ClassName}.g.cs", model.GenerateCode().ToCodeString());
         }
-
-        if (model.FlagBytes > 0)
+        catch (Exception ex)
         {
-          var isNetworkUpdate = new StringBuilder("base.IsNetworkUpdate()");
-          for (var i = 0; i < model.FlagBytes; i++)
-          {
-            isNetworkUpdate.Append($" || _CustomUpdateFlags[{i}] > 0");
-          }
-          source.Append($@"
-    private readonly byte[] _CustomUpdateFlags = new byte[{model.FlagBytes}];
-
-    public override bool IsNetworkUpdate() => {isNetworkUpdate};
-
-    public override void BuildUpdate(RocketBinaryWriter writer, ushort networkUpdateType)
-    {{
-      base.BuildUpdate(writer, networkUpdateType);");
-          for (var i = 0; i < model.FlagBytes; i++)
-            source.Append($@"
-      writer.WriteByte(_CustomUpdateFlags[{i}]);");
-          foreach (var prop in model.Props)
-            source.Append(prop.GenerateBuildUpdate());
-          for (var i = 0; i < model.FlagBytes; i++)
-            source.Append($@"
-      _CustomUpdateFlags[{i}] = 0;");
-          source.Append($@"
-    }}
-
-    public override void ProcessUpdate(RocketBinaryReader reader, ushort networkUpdateType)
-    {{
-      base.ProcessUpdate(reader, networkUpdateType);");
-          for (var i = 0; i < model.FlagBytes; i++)
-            source.Append($@"
-      _CustomUpdateFlags[{i}] = reader.ReadByte();");
-          foreach (var prop in model.Props)
-            source.Append(prop.GenerateProcessUpdate());
-          source.Append($@"
-    }}
-
-    public override void SerializeOnJoin(RocketBinaryWriter writer)
-    {{
-      base.SerializeOnJoin(writer);");
-          foreach (var prop in model.Props)
-            source.Append(prop.GenerateSerializeOnJoin());
-          source.Append($@"
-    }}
-
-    public override void DeserializeOnJoin(RocketBinaryReader reader)
-    {{
-      base.DeserializeOnJoin(reader);");
-          foreach (var prop in model.Props)
-            source.Append(prop.GenerateDeserializeOnJoin());
-          source.Append($@"
-    }}");
+          using (var writer = new StreamWriter("error.log")) writer.Write($"{ex.Message}\n{ex.StackTrace}");
+          throw;
         }
-
-        if (model.SaveDataType != null)
-        {
-          source.Append($@"
-
-    public class {model.ClassName}_SaveData : {model.SaveDataType}
-    {{");
-          foreach (var prop in model.Props)
-            source.Append(prop.GenerateSaveDataProp());
-          source.Append($@"
-    }}
-
-    public override ThingSaveData SerializeSave()
-    {{
-      ThingSaveData saveData = new {model.ClassName}_SaveData();
-      this.InitialiseSaveData(ref saveData);
-      return saveData;
-    }}
-
-    protected override void InitialiseSaveData(ref ThingSaveData baseData)
-    {{
-      base.InitialiseSaveData(ref baseData);
-      if (baseData is not {model.ClassName}_SaveData saveData)
-        return;");
-          foreach (var prop in model.Props)
-            source.Append(prop.GenerateSerializeSave());
-          source.Append($@"
-    }}
-
-    public override void DeserializeSave(ThingSaveData baseData)
-    {{
-      base.DeserializeSave(baseData);
-      if (baseData is not {model.ClassName}_SaveData saveData)
-        return;");
-          foreach (var prop in model.Props)
-            source.Append(prop.GenerateDeserializeSave());
-          source.Append($@"
-    }}");
-
-          if (model.ConnectedReferences)
-          {
-            source.Append($@"
-
-    public override void OnFinishedLoad()
-    {{");
-            foreach (var prop in model.Props)
-              source.Append(prop.GenerateOnFinishedLoad());
-            source.Append($@"
-    }}");
-          }
-        }
-
-        source.Append($@"
-  }}");
-        if (model.Namespace != "")
-          source.Append($@"
-}}");
-        context.AddSource($"{model.Namespace}.{model.ClassName}.g.cs", source.ToString());
       });
     }
 
-    private static EquatableList<InterfaceProp> GetProps(TypedConstant itype)
+    private static EquatableList<ConnectedProperty> GetProps(TypedConstant itype)
     {
       if (itype.Value is not INamedTypeSymbol { TypeKind: TypeKind.Interface } ifaceSymbol)
         throw new Exception($"{itype} is not an interface");
-      var res = new EquatableList<InterfaceProp>();
+      var res = new EquatableList<ConnectedProperty>();
       var networkCount = 0;
       foreach (var prop in ifaceSymbol.GetMembers().OfType<IPropertySymbol>())
       {
@@ -275,7 +148,7 @@ namespace {model.Namespace}
           else if (fqn == "LaunchPadBooster.Generated.NetworkedAttribute")
             networked = true;
         }
-        res.Add(new InterfaceProp
+        res.Add(new ConnectedProperty
         {
           DebugComments = comments,
           TypeName = prop.Type.ToDisplayString(),
@@ -289,167 +162,6 @@ namespace {model.Namespace}
           networkCount++;
       }
       return res;
-    }
-
-    private record ConnectedClass(string Namespace, string ClassName, string SaveDataType, EquatableList<InterfaceProp> Props, int FlagBytes, bool ConnectedReferences);
-
-    private struct InterfaceProp
-    {
-      public List<string> DebugComments;
-      public string TypeName;
-      public string PropName;
-      public bool Saved;
-      public bool Networked;
-      public bool Referencable;
-      public int NetworkIndex;
-
-      public string GenerateProperty()
-      {
-        var source = new StringBuilder();
-        foreach (var comment in DebugComments ?? new())
-          source.Append($"\n//{comment}");
-        if (Referencable)
-          source.Append($@"
-    private long _saved{PropName};");
-        source.Append($@"
-    private {TypeName} _{PropName};
-    public {TypeName} {PropName}
-    {{
-      get => _{PropName};
-      set
-      {{
-        if (Comparators.IsEqual(_{PropName}, value))
-          return;
-        _{PropName} = value;
-");
-
-        if (Networked)
-        {
-          var bindex = NetworkIndex / 8;
-          var bit = 1 << (NetworkIndex & 7);
-          source.Append(@$"
-        if (NetworkManager.IsServer)
-          _CustomUpdateFlags[{bindex}] |= {bit};
-");
-        }
-
-        source.Append(@$"
-      }}
-    }}
-");
-        return source.ToString();
-      }
-
-      public string GenerateBuildUpdate()
-      {
-        if (!Networked)
-          return "";
-
-        var bindex = NetworkIndex / 8;
-        var bit = 1 << (NetworkIndex & 7);
-
-        return $@"
-      if ((_CustomUpdateFlags[{bindex}] & {bit}) != 0)
-        Serializers.Serialize(writer, _{PropName});";
-      }
-
-      public string GenerateProcessUpdate()
-      {
-        if (!Networked)
-          return "";
-
-        var bindex = NetworkIndex / 8;
-        var bit = 1 << (NetworkIndex & 7);
-
-        return $@"
-      if ((_CustomUpdateFlags[{bindex}] & {bit}) != 0)
-        Serializers.Deserialize(reader, out _{PropName});";
-      }
-
-      public string GenerateSerializeOnJoin()
-      {
-        if (!Networked)
-          return "";
-
-        return $@"
-      Serializers.Serialize(writer, _{PropName});";
-      }
-
-      public string GenerateDeserializeOnJoin()
-      {
-        if (!Networked)
-          return "";
-
-        if (Referencable)
-          return $@"
-      Serializers.Deserialize(reader, out _saved{PropName});";
-        return $@"
-      Serializers.Deserialize(reader, out _{PropName});";
-      }
-
-      public string GenerateSaveDataProp()
-      {
-        if (!Saved)
-          return "";
-        if (Referencable)
-          return $@"
-      [XmlElement]
-      public long {PropName};";
-        return $@"
-      [XmlElement]
-      public {TypeName} {PropName};";
-      }
-
-      public string GenerateSerializeSave()
-      {
-        if (!Saved)
-          return "";
-        if (Referencable)
-          return $@"
-      saveData.{PropName} = _{PropName}?.ReferenceId ?? 0;";
-        return $@"
-      saveData.{PropName} = _{PropName};";
-      }
-
-      public string GenerateDeserializeSave()
-      {
-        if (!Saved)
-          return "";
-        if (Referencable)
-          return $@"
-      _saved{PropName} = saveData.{PropName};";
-        return $@"
-      _{PropName} = saveData.{PropName};";
-      }
-
-      public string GenerateOnFinishedLoad()
-      {
-        if (!Referencable || !(Saved || Networked))
-          return "";
-        return $@"
-      _{PropName} = Referencable.Find<{TypeName}>(_saved{PropName});";
-      }
-    }
-
-    // copied from https://github.com/dotnet/roslyn/blob/main/docs/features/incremental-generators.cookbook.md
-    private class EquatableList<T> : List<T>, IEquatable<EquatableList<T>>
-    {
-      public bool Equals(EquatableList<T> other)
-      {
-        if (other is null || this.Count != other.Count)
-          return false;
-
-        for (var i = 0; i < this.Count; i++)
-          if (!EqualityComparer<T>.Default.Equals(this[i], other[i]))
-            return false;
-        return true;
-      }
-
-      public override bool Equals(object obj) => Equals(obj as EquatableList<T>);
-      public override int GetHashCode() => this.Select(item => item?.GetHashCode() ?? 0).Aggregate((x, y) => x ^ y);
-      public static bool operator ==(EquatableList<T> list1, EquatableList<T> list2) =>
-        ReferenceEquals(list1, list2) || list1 is not null && list2 is not null && list1.Equals(list2);
-      public static bool operator !=(EquatableList<T> list1, EquatableList<T> list2) => !(list1 == list2);
     }
   }
 }
