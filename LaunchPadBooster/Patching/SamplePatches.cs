4fdd41c54d9d21559f7647f8588ac33b9c78ba57
Spacebuilder2020 <spacebuilder2020@users.noreply.github.com>
2025-10-24 18:23:07 -0600

    Add an example patch to showcase how to use the logic. This is configured so it will never get applied on a modern stationeers version.

---
+++ LaunchPadBooster/Patching/SamplePatches.cs
@@ -0,0 +1,17 @@
+using Assets.Scripts.Objects;
+using HarmonyLib;
+using UnityEngine;
+
+namespace LaunchPadBooster.Patching
+{
+    [GameVersion("0.0.0.0", "0.2.9999.99999")]
+    public static class SamplePatches
+    {
+        [GameVersion("0.0.0.0", "0.2.0.0")]
+        [HarmonyPatch(typeof(Item), nameof(Item.OnUseItem)), HarmonyPostfix]
+        static void Item_OnUseItem(ref Thing __result, Thing useOnThing)
+        {
+            Debug.Log(__result.DisplayName);
+        }
+    }
+}
