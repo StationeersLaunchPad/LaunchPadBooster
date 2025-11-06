2dea4811b78b310a6921ccd797b408ad984ad598
Spacebuilder2020 <spacebuilder2020@users.noreply.github.com>
2025-11-06 15:44:41 -0700

    Update patch code to grab current branch from Steamworks

--- LaunchPadBooster/Patching/HarmonyGameBranchPatch.cs
+++ LaunchPadBooster/Patching/HarmonyGameBranchPatch.cs
@@ -3,12 +3,14 @@ using System.Linq;
 
 namespace LaunchPadBooster.Patching
 {
-    // TODO: Disable until a way to get the current branch is determined
-    // [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
     public class HarmonyGameBranchPatch : HarmonyConditionalPatch
     {
         public readonly String[] Branches;
-        public static readonly String CurrentBranch = "public";
+        
+        //Note: the reason for using `public` for the case where the branch is not specified is
+        //because when downloading a branch with steamcmd, when you specify `-beta public`, it will download the main branch
+        public static readonly String CurrentBranch = Steamworks.SteamApps.CurrentBetaName ?? "public";
         
         public override string Description => $"Current: {CurrentBranch} Branches: [{string.Join(",", Branches)}]";
         public HarmonyGameBranchPatch(String[] branches) : 
