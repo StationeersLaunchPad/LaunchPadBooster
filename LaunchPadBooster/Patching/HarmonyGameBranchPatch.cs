fa8a27e86f8d497cff861d4dd276190f658f28da
Spacebuilder2020 <spacebuilder2020@users.noreply.github.com>
2025-11-23 13:29:42 -0700

    Add acf manifest parsing to get beta branch

--- LaunchPadBooster/Patching/HarmonyGameBranchPatch.cs
+++ LaunchPadBooster/Patching/HarmonyGameBranchPatch.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using System.Linq;
 
 namespace LaunchPadBooster.Patching
@@ -12,7 +13,22 @@ namespace LaunchPadBooster.Patching
     //because when downloading a branch with steamcmd, when you specify `-beta public`, it will download the main branch
 
     private static string _currentBranch;
-    public static string CurrentBranch => _currentBranch ??= Steamworks.SteamApps.CurrentBetaName ?? "public";
+
+    private static string GetBetaBranchFromAcf()
+    {
+      var acfPath = Path.Combine(StationSaveUtils.ExeDirectory.FullName, "steamapps", "appmanifest_544550.acf");
+      if (!File.Exists(acfPath)) return null;
+      var acf = string.Join("\n", File.ReadAllLines(acfPath));
+      var start = acf.IndexOf("\"UserConfig\"", StringComparison.Ordinal);
+      start = acf.IndexOf("\"BetaKey\"", start, StringComparison.Ordinal);
+      start = acf.IndexOf("\"", start+1, StringComparison.Ordinal);
+      var end = acf.IndexOf("}", start, StringComparison.Ordinal);
+      if (start == -1 || end == -1) return null;
+      var branch = acf.Substring(start, end - start).Replace("\"", "").Trim();
+      return branch;
+    }
+    
+    public static string CurrentBranch => _currentBranch ??= GetBetaBranchFromAcf() ?? Steamworks.SteamApps.CurrentBetaName ?? "public";
 
     public override bool CanPatch => this.Branches.Contains(CurrentBranch);
     public override string Description => $"Current: {CurrentBranch} Branches: [{string.Join(",", this.Branches)}]";
