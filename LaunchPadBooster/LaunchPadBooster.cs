9cdf69f16bbccf60b1d524a850f372c4c35aec55
Tom Holmes <tholmes150@gmail.com>
2025-11-30 18:00:37 -0600

    bump version to 0.1.8

--- LaunchPadBooster/LaunchPadBooster.cs
+++ LaunchPadBooster/LaunchPadBooster.cs
@@ -4,6 +4,6 @@ namespace LaunchPadBooster
   {
     public const string NAME = "LaunchPadBooster";
     public const string GUID = "launchpad.booster";
-    public const string VERSION = "0.1.7";
+    public const string VERSION = "0.1.8";
   }
 }
