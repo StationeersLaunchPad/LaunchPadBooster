e42dc5efa2eb8afc1d7e944e1a542c97e36a9c01
ilodev <ilodev@users.noreply.github.com>
2026-08-13 23:44:03 +0200

    Create LogicNetworkType.cs

---
+++ LaunchPadBooster/Logic/LogicNetworkType.cs
@@ -0,0 +1,16 @@
+namespace LaunchPadBooster.Logic;
+
+public enum LogicNetworkType : byte
+{
+    Byte = 0,
+    UInt16 = 1,
+    UInt32 = 2,
+    Int64 = 3,
+    Half = 4,
+    Single = 5,
+    Double = 6,
+    Int32 = 7,
+    SByte = 8,
+    Normalized = 9,
+    UInt64 = 10
+}
